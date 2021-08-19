4935eedaf1c1bc5a77638170972ad0eddc41bcd2
Diasel1982 <diasel99@outlook.com>
2021-08-19 15:21:54 +0200

    first Version

--- teska_monitor/monitor/app.py
+++ teska_monitor/monitor/app.py
@@ -6,28 +6,35 @@ app = Flask(__name__)
 
 TEMPLATE ="""
 <h1>Monitoring</h1>
-<p><strong>CPU_Nutzung: </strong>{cpu}</p>
-<p><strong>Virtueller Ram: </strong>{mem}</p>
+<p><strong>CPU Nutzung: </strong>{cpu}%</p>
+<p><strong>Virtual Memory: </strong>{mem}%</p>
+<p><strong>Total Memory: </strong>{total} GB</p>
 """
 
 @app.route("/")
 def index():
    
-    telper = telemetry.get_cpu_percent()
-    telper = str(telper)
+    # telper = telemetry.get_cpu_percent()
+    # telper = str(telper)
 
-    telmem = telemetry.get_virtual_memory()
-    telmem = str(telmem)    
+    # telmem = telemetry.get_virtual_memory()
+    # telmem = str(telmem)    
    
+    data = telemetry.get_all()
 
-    return TEMPLATE.format(cpu=telper, mem=telmem)
+    total = data["total_memory"]
+    total_gb = round(total / 1024 / 1024 / 1024, 2)
+    
+    return TEMPLATE.format(cpu=data["cpu_usage"], mem=data["virtual_memory"], total=total_gb)
 
 
 @app.route("/json")
 def show_json():
-    telper = telemetry.get_cpu_percent()  
-    telmem = telemetry.get_virtual_memory()  
-    output = {"cpu_usage": telper, "Virtueller Ram": telmem}
+    # telper = telemetry.get_cpu_percent()  
+    # telmem = telemetry.get_virtual_memory()  
+    # output = {"cpu_usage": telper, "Virtueller Ram": telmem}
+
+    output = telemetry.get_all()
 
     return output
 
